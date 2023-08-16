c5529654193db3b97019a26f3e8b5d128d85160c
Jayjeet1013 <jayjeet1013@gmail.com>
2023-08-16 23:03:25 +0530

    demo part completed

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import {copy,linkIcon,loader,tick} from '../assets';
 
 const Demo = () => {
+  const [article,setArticle] =useState({
+   url:"",
+   article:"",
+  });
+
+const handleSubmit =async (e) => {
+ alert("Submitted");
+}
+
   return (
-    <div>Demo</div>
+    <section className='w-full mt-16 max-w-xl '>
+<div className='flex flex-col w-full gap-2'>
+<form className='relative flex justify-center items-center' onSubmit={handleSubmit}>
+<img src={linkIcon} alt='linkIcon'  className='absolute left-0 my-2 ml-3 w-5'/>
+
+<input type='url' placeholder='Enter Your Url' value={article.url} onChange={(e) => setArticle({...article,url: e.target.value})} required className='url_input peer' />
+<button type='submit' className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'> 
+↵
+</button>
+</form>
+</div>
+    </section>
   )
 }
 
